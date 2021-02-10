de2dc7988f949a54c2c62de3fe3d508a908a2e6b
artursBiezbardis <arturste2007@gmail.com>
2021-02-10 15:44:45 +0200

    validation price and sku

--- app/Services/AddProductService.php
+++ app/Services/AddProductService.php
@@ -9,35 +9,6 @@ use App\TypeModelCollection;
 
 class AddProductService
 {
-    /*public function saveProduct():void
-    {
-        $skuExist = (new CheckIfSkuExistInDBRepository())->execute($_POST['sku']);
-
-        if (!$skuExist) {
-            $_POST = array_filter($_POST);
-            $descriptionValueArray = array_diff_key($_POST,
-                array_flip(['sku', 'name', 'price', 'select']));
-            foreach ((new TypeModelCollection())->getTypeModels() as $key => $model) {
-                if ($_POST['select'] == $key) {
-                    $_POST['description'] = $model->formattingProductDescription($descriptionValueArray);
-                }
-            }
-            $_POST['price'] = (int)($_POST['price'] * 100);
-            $model = (new Product(
-                $_POST['sku'], $_POST['name'], $_POST['price'], $_POST['description']));
-
-            (new AddProductRepository())->save($model);
-
-        } else{
-            header('Location:/product/add');
-            foreach ($_POST as $key => $item) {
-                if ($item != '') {
-                    $_SESSION[$key] = $item;
-                }
-
-            }
-        }
-    }*/
     public function saveProduct(): void
     {
         $skuExist = (new CheckIfSkuExistInDBRepository())->execute($_POST['sku']);
@@ -64,8 +35,6 @@ class AddProductService
                 $_POST['sku'], $_POST['name'], $_POST['price'], $_POST['description']));
 
             (new AddProductRepository())->save($model);
-
-
         }
     }
 
